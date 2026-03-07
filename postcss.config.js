5f43a541914f3d1ea762f3b61312e4d715eac353
loki-the-god <loiclejarragapindard@outlook.com>
2026-03-07 20:14:38 +0100

    Added PostCSS

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+export default {
+    plugins: {
+        "@tailwindcss/postcss": {},
+        "postcss-preset-env": {
+            stage: 3,
+            features: {
+                "nesting-rules": true
+            }
+        },
+        cssnano: { preset: "default" }
+    }
+}
